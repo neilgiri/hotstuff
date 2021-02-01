64b9b40f117758028a4e7ba1db266f2dbcc207dd
neilgiri <giridhn@berkeley.edu>
2021-01-31 18:22:13 -0800

    added separate machine for clients

--- profile.py
+++ profile.py
@@ -15,7 +15,7 @@ pc = portal.Context()
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
 
-num_nodes = 4
+num_nodes = 5
 lan = request.LAN()
 
 for i in range(num_nodes):
